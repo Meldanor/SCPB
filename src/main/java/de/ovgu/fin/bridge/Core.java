ab987e37754017b623df27358b07ad23362991f6
Kilian Gärtner <herr.kilian.gaertner@gmail.com>
2017-11-06 13:47:34 +0100

    Initial commit

---
+++ src/main/java/de/ovgu/fin/bridge/Core.java
@@ -0,0 +1,10 @@
+package de.ovgu.fin.bridge;
+
+/**
+ * Created on 06.11.2017.
+ */
+public class Core {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
